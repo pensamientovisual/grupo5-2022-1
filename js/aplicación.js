4e017781f139b0d492f1e8c7332c9cd161d419f8
inecalvimontes <ines.calvimontes@uc.cl>
2022-04-26 00:28:07 -0400

    guardadoo

--- js/aplicación.js
+++ js/aplicación.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
   var d4j = $("#alerce");
   var d9j = $("#d9");
+  var d12j = $("#d12");
 
   d4j.click(function(){
       $(this).fadeTo("fast", 0);
@@ -14,4 +15,8 @@ $(document).ready(function(){
   d4j.click(function(){
       $(d9j).show('fast');
   });
+
+  d4j.click(function(){
+    $(d12j).hide('fast');
+});
 })
