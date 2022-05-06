936aaa43df4fdda9f6f9798aea27f0a112968702
inecalvimontes <ines.calvimontes@uc.cl>
2022-05-06 16:59:20 -0400

    mensaje

--- js/aplicación.js
+++ js/aplicación.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
   var pajaro = $(".fotopajaro")
-  var condor = $("#fotocondor")
 
   pajaro.mouseenter(function(){
       $(this).fadeTo("fast",1);
@@ -39,25 +38,13 @@ $(document).ready(function(){
     $("#concon").hide('fast');
     $("#cernicalo").hide('fast');
     $("#tordo").hide('fast');
-<<<<<<< HEAD
     $("#huet huet").hide('fast');
-=======
-    $("#huethuet").hide('fast');
->>>>>>> 4819ff6a44fbb9c33e0f04a3472a4d3f8db8ee29
     $("#diuca").hide('fast');
     $("#chincol").hide('fast');
     $("#rayadito").hide('fast');
     $("#chercan").hide('fast');
     $("#picaflor").hide('fast');
     $("#cachudito").hide('fast');
-<<<<<<< HEAD
-=======
   });
-
-  $("#fotogarzacuca").click(function(){
-    $("#condor").hide('fast');
-    $("#garza").show('fast');
->>>>>>> 4819ff6a44fbb9c33e0f04a3472a4d3f8db8ee29
-  });
-
+  
 })
