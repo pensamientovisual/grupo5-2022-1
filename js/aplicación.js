4819ff6a44fbb9c33e0f04a3472a4d3f8db8ee29
berniliral <mbliralc.cl>
2022-05-06 16:46:40 -0400

    hola

--- js/aplicación.js
+++ js/aplicación.js
@@ -18,6 +18,34 @@ $(document).ready(function(){
   $("#fotocondor").click(function(){
     $("#condor").show('fast');
     $("#garza").hide('fast');
+    $("#huala"). hide('fast');
+    $("#aguila").hide('fast');
+    $("#bandurria").hide('fast');
+    $("#cormoran").hide('fast');
+    $("#quetru").hide('fast');
+    $("#caiquen").hide('fast');
+    $("#caranca").hide('fast');
+    $("#peuco").hide('fast');
+    $("#tagua").hide('fast');
+    $("#aguilucho").hide('fast');
+    $("#pato real").hide('fast');
+    $("#tucuquere").hide('fast');
+    $("#carpintero").hide('fast');
+    $("#pilpilen").hide('fast');
+    $("#martin pescador").hide('fast');
+    $("#cortacorriente").hide('fast');
+    $("#choroy").hide('fast');
+    $("#torcaza").hide('fast');
+    $("#concon").hide('fast');
+    $("#cernicalo").hide('fast');
+    $("#tordo").hide('fast');
+    $("#huethuet").hide('fast');
+    $("#diuca").hide('fast');
+    $("#chincol").hide('fast');
+    $("#rayadito").hide('fast');
+    $("#chercan").hide('fast');
+    $("#picaflor").hide('fast');
+    $("#cachudito").hide('fast');
   });
 
   $("#fotogarzacuca").click(function(){
