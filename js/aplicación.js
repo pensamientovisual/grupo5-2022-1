036b00b5df0f30ce5c2caa654de58d0ed9a036c8
inecalvimontes <ines.calvimontes@uc.cl>
2022-05-06 16:54:32 -0400

    mensaje

--- js/aplicación.js
+++ js/aplicación.js
@@ -18,11 +18,34 @@ $(document).ready(function(){
   $("#fotocondor").click(function(){
     $("#condor").show('fast');
     $("#garza").hide('fast');
-  });
-
-  $("#fotogarzacuca").click(function(){
-    $("#condor").hide('fast');
-    $("#garza").show('fast');
+    $("#huala"). hide('fast');
+    $("#aguila").hide('fast');
+    $("#bandurria").hide('fast');
+    $("#cormoran").hide('fast');
+    $("#quetru").hide('fast');
+    $("#caiquen").hide('fast');
+    $("#caranca").hide('fast');
+    $("#peuco").hide('fast');
+    $("#tagua").hide('fast');
+    $("#aguilucho").hide('fast');
+    $("#pato real").hide('fast');
+    $("#tucuquere").hide('fast');
+    $("#carpintero").hide('fast');
+    $("#pilpilen").hide('fast');
+    $("#martin pescador").hide('fast');
+    $("#cortacorriente").hide('fast');
+    $("#choroy").hide('fast');
+    $("#torcaza").hide('fast');
+    $("#concon").hide('fast');
+    $("#cernicalo").hide('fast');
+    $("#tordo").hide('fast');
+    $("#huet huet").hide('fast');
+    $("#diuca").hide('fast');
+    $("#chincol").hide('fast');
+    $("#rayadito").hide('fast');
+    $("#chercan").hide('fast');
+    $("#picaflor").hide('fast');
+    $("#cachudito").hide('fast');
   });
 
 })
